63083054ea80fc71bd75ded88d5dbdc26646f22d
kystyn <konstantindamaskinskiy@gmail.com>
2020-12-06 00:42:15 +0300

    Add plot eps(n)]

--- src/degenmat.py
+++ src/degenmat.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import pyplot as pl
 
 def mid( n, eps ):
     A = np.zeros((n, n))
@@ -22,27 +23,42 @@ def rad( n, eps ):
     return A
 
 
-n = int(input('Input matrix size: '))
+#n = int(input('Input matrix size: '))
 
-l = 0
-r = 200
+def evaluate( n ):
+    l = 0
+    r = 200
 
-accuracy = 1e-3
+    accuracy = 1e-3
 
-while r - l > accuracy:
-    eps = (l + r) / 2
+    while r - l > accuracy:
+        eps = (l + r) / 2
 
-    invA = np.abs(np.linalg.inv(mid(n, eps)))
-    radA = rad(n, eps)
-    rho = np.max(np.linalg.eigvals(invA.dot(radA)))
+        invA = np.abs(np.linalg.inv(mid(n, eps)))
+        radA = rad(n, eps)
+        rho = np.max(np.linalg.eigvals(invA.dot(radA)))
 
-    # special, need low
-    if rho > 1:
-        r = eps
-    # not-special, need up
-    else:
-        l = eps
+        # special, need low
+        if rho > 1:
+            r = eps
+        # not-special, need up
+        else:
+            l = eps
+    return eps
 
 
-print('bound epsilon is: ', eps)
+x = []
+y = []
+eps = 0
+for n in range(2, 15):
+    x.append(n)
+    eps = evaluate(n)
+    y.append(eps)
+    print('bound epsilon is: ', eps)
 
+pl.plot(x, y)
+pl.title('Dependence of $\epsilon$ on matrix dimension')
+pl.xlabel('n')
+pl.ylabel('$\epsilon$')
+pl.savefig('../figure/degenmat.png', format='png')
+pl.show()
