265c891aa56f08e6c46787bca4cb65bfd8e4c915
kystyn <konstantindamaskinskiy@gmail.com>
2021-01-07 15:07:32 +0300

    v2

--- src/regularization/main.py
+++ src/regularization/main.py
@@ -10,30 +10,36 @@ print(A, inf_b, sup_b)
 
 [tolmax, argmax, envs, ccode] = tolsolvty(A, A, inf_b, sup_b)
 
-print("max Tol = ", tolmax)
+print("max Tol = ", tolmax[0])
+oldtolmax = tolmax[0]
 
 diag_rad_b = np.diag(np.squeeze(np.asarray(0.5 * (sup_b - inf_b))))
 
 C = np.block([[-A, -diag_rad_b],
               [A, -diag_rad_b]])
+print("C = ", C)
 
 mid_b = 0.5 * (inf_b + sup_b)
 r = np.concatenate([-mid_b, mid_b])
-r = np.squeeze(np.asarray(r))
 
-pos_sign = (0, None)
-any_sign = (None, None)
+pos_sign = (0, +np.inf)
+any_sign = (-np.inf, +np.inf)
+str_sign = (0, -tolmax[0])
+
 res = opt.linprog([0, 0, 1, 1, 1],
                       A_ub=C,
                       b_ub=r,
                       bounds=[any_sign, any_sign,
                               pos_sign, pos_sign, pos_sign],
-                      method='simplex')
-print(res.x)
+                      method='revised simplex')
+print('res = ', res.x)
+print('status: ', res.status)
 
-inf_b -= res.x[-3:].reshape(3, 1)
-sup_b += res.x[-3:].reshape(3, 1)
-
-[tolmax, argmax, envs, ccode] = tolsolvty(A, A, inf_b, sup_b)
+inf_b -= np.asarray([-tolmax[0]] * 3).reshape(3, 1)#res.x[-3:].reshape(3, 1)
+sup_b += np.asarray([-tolmax[0]] * 3).reshape(3, 1)#res.x[-3:].reshape(3, 1)
+[tolmax, argmax, _, __] = tolsolvty(A, A, inf_b, sup_b)
 
 print("max Tol = ", tolmax, "argmax = ", argmax)
+
+xx = np.concatenate((argmax, np.asarray([0, -oldtolmax, 0]).reshape(3, 1)), axis=0)
+print("Cx - r = ", np.matmul(C, xx) - r)
